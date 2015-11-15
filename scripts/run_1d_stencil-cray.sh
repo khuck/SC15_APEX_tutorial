b379e4220e63bd6bdb690abe0ab2733db88f1c71
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-15 09:55:12 -0800

    removing call to get_hostfile

--- scripts/run_1d_stencil-cray.sh
+++ scripts/run_1d_stencil-cray.sh
@@ -10,7 +10,6 @@ export TAU_TRACE=1
 export TAU_PROFILE=1
 #TAU_PROFILE_FORMAT=merged
 
-get_hostfile 
 cmd="aprun -N 1 -n 1 -d ${i} ./build-cray/apex_examples/1d_stencil_4 --hpx:threads $i --nx $nx --np $np --nt $nt"
 echo ${cmd}
 ${cmd}
