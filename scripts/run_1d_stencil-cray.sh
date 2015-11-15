d35142153a7ae4984fa5deff1938948f136867f8
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-15 09:53:31 -0800

    adding cray 1dstencil version

--- scripts/run_1d_stencil-cray.sh
+++ scripts/run_1d_stencil-cray.sh
@@ -2,22 +2,16 @@
 
 nx=100000
 np=1000
-nt=45
-i=48
+nt=100
+i=24
 
-rm -f apex.conf tau.conf
-cat << EOF > apex.conf
-APEX_TAU=1
-EOF
-cat << EOF > tau.conf
-TAU_TRACE=1
-TAU_PROFILE=1
-EOF
+export APEX_TAU=1
+export TAU_TRACE=1
+export TAU_PROFILE=1
 #TAU_PROFILE_FORMAT=merged
 
-cmd="aprun -N 1 -n 1 -d ${i} -j 2 ./build-cray/apex_examples/1d_stencil_4 --hpx:threads $i --nx $nx --np $np --nt $nt"
+get_hostfile 
+cmd="aprun -N 1 -n 1 -d ${i} ./build-cray/apex_examples/1d_stencil_4 --hpx:threads $i --nx $nx --np $np --nt $nt"
 echo ${cmd}
 ${cmd}
-
-rm -f apex.conf tau.conf
 
