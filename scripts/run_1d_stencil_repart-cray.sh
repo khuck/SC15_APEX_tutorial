cda5a32735ff57617085e6c439aeee5c351084b1
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-15 10:03:15 -0800

    fixing bug

--- scripts/run_1d_stencil_repart-cray.sh
+++ scripts/run_1d_stencil_repart-cray.sh
@@ -12,7 +12,7 @@ export APEX_TAU=1
 export TAU_PROFILE_FORMAT=merged
 export APEX_POLICY=1
 
-cmd="aprun -N 1 -n 1 -d 12 ./build-host/apex_examples/1d_stencil_4_repart --hpx:print-counter /threadqueue/length --hpx:print-counter-interval 100 --hpx:print-counter-destination /dev/null --hpx:threads 12 --nx $nx --nr $nr --nt $nt"
+cmd="aprun -N 1 -n 1 -d 12 ./build-cray/apex_examples/1d_stencil_4_repart --hpx:print-counter /threadqueue/length --hpx:print-counter-interval 100 --hpx:print-counter-destination /dev/null --hpx:threads 12 --nx $nx --nr $nr --nt $nt"
 echo ${cmd}
 ${cmd}
 mv tauprofile.xml tauprofile-repart.xml
